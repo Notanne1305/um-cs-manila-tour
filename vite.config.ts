3430cb0dc4575a9462f30bc74082bac88ee6c2af
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2026-01-27 23:09:17 +0800

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/my-project/", // Replace 'my-project' with your repository name
+  base: "/", // Replace 'my-project' with your repository name
 }));
